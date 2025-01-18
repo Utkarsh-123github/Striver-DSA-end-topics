f95f2185ef53117e0d23afe6c6fca5853f48e89d
Utkarsh Tiwari <utk0703@gmail.com>
2025-01-18 13:12:36 +0530

    Hand of Straight

---
+++ Heaps/06_Hand_of_straights.cpp
@@ -0,0 +1,48 @@
+// Alice has some number of cards and she wants to rearrange the cards into groups so that each group is of size groupSize, and consists of groupSize consecutive cards.
+
+// Given an integer array hand where hand[i] is the value written on the ith card and an integer groupSize, return true if she can rearrange the cards, or false otherwise.
+
+// Example 1:
+
+// Input: hand = [1,2,3,6,2,3,4,7,8], groupSize = 3
+// Output: true
+// Explanation: Alice's hand can be rearranged as [1,2,3],[2,3,4],[6,7,8]
+// Example 2:
+
+// Input: hand = [1,2,3,4,5], groupSize = 4
+// Output: false
+// Explanation: Alice's hand can not be rearranged into groups of 4.
+
+#include<bits/stdc++.h>
+using namespace std;
+
+bool findSuccessors(vector<int>&hand,int groupSize, int i, int n){
+    int next = hand[i]+1;
+    hand[i] = -1; // Mark as used
+    int count = 1;
+    i = i+1;
+    while(i<n && count<groupSize){
+        if(hand[i] ==  next){
+            next = hand[i]+1;
+            hand[i] = -1;
+            count++;
+        }
+        i++;
+    }
+    return count == groupSize;
+}
+
+bool isNStraightHand(vector<int>& hand, int groupSize) {
+    int n = hand.size();
+    if(n%groupSize != 0)return false;
+    sort(hand.begin(),hand.end());
+    for(int i=0;i<n;i++){
+        if(hand[i]>=0){
+            if(!findSuccessors(hand,groupSize,i,n))return false;
+        }
+    }
+    return true;
+}
+
+// TC = O(nlogn)
+// SC = O(1)
