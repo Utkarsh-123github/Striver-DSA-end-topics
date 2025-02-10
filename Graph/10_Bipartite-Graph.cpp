eed9b8cac066feeb7087c5107d62040984240101
Utkarsh Tiwari <utk0703@gmail.com>
2025-02-10 23:34:02 +0530

    Updated

--- Graph/10_Bipartite-Graph.cpp
+++ Graph/10_Bipartite-Graph.cpp
@@ -24,6 +24,14 @@
 // In easy words, check if the graph has a cycle with odd number of nodes, if it is, then the graph is not bipartite
 // Also linear graphs are always bipartite
 
+
+// The algorithm steps are as follows:
+
+// For DFS traversal, we need a start node and a visited array but in this case, instead of a visited array, we will take a colour array where all the nodes are initialised to -1 indicating they are not coloured yet.
+// In the DFS function call, make sure to pass the value of the assigned colour, and assign the same in the colour array. We will try to colour with 0 and 1, but you can choose other colours as well. We will start with the colour 0, you can start with 1 as well, just make sure for the adjacent node, it should be opposite of what the current node has. 
+// In DFS traversal, we travel in-depth to all its uncoloured neighbours using the adjacency list. For every uncoloured node, initialise it with the opposite colour to that of the current node.
+// If at any moment, we get an adjacent node from the adjacency list which is already coloured and has the same colour as the current node, we can say it is not possible to colour it, hence it cannot be bipartite. Thereby return a false indicating the given graph is not bipartite; otherwise, keep on returning true.
+
 #include<bits/stdc++.h>
 using namespace std;
 
